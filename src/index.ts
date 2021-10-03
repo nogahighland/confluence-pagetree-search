b2f071db8994b4b2f9e114d64321c39f810588ec
nogahighland <noga.highland@gmail.com>
2021-10-03 18:52:39 +0900

    open blank pages

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import App from './components/app.vue'
-console.log(App)
+import { addTargetBlank } from './lib/'
+
+import Vue, { VNode } from 'vue'
+const appElement = document.createElement('div')
+appElement.id = 'page-tree-incremental-search'
+const nav = document.querySelector('.acs-nav-sections')
+
+if (nav && nav.parentNode) {
+  addTargetBlank()
+  nav.parentNode.insertBefore(appElement, nav.nextElementSibling)
+
+  const app = new Vue({
+    render: (h): VNode => h(App)
+  }).$mount()
+
+  appElement.appendChild(app.$el)
+}
---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+const host = location.host
+
+export const addTargetBlank = (): void => {
+  document.querySelectorAll('a').forEach(a => {
+    try {
+      if (a.href && host != new URL(a.href).host) {
+        a.target = '_blank'
+      }
+    } catch (e) {
+      console.error(a, e)
+    }
+  })
+}
