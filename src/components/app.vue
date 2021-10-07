88e3b424e1a6bbd57da92eed09375f2afe3be66a
nogahighland <noga.highland@gmail.com>
2021-10-08 01:03:57 +0900

    add suggestion compnent

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div
   input(@input='onInput')
-  li(v-for='(node, i) in nodeList' :key='i') {{ node.linkText }}
+  suggestion(v-for='(node, i) in nodeList' :key='i' :node='node')
 </template>
 
 <script lang="ts">
@@ -9,11 +9,14 @@ div
 
 import { Component, Vue } from 'vue-property-decorator'
 
+import Suggestion from '@/components/suggestion.vue'
 import { completions } from '@/store/completions'
 import { pageTree } from '@/store/page-tree'
 import { Node } from '@/types'
 
-@Component
+@Component({
+  components: { Suggestion }
+})
 export default class App extends Vue {
   mounted(): void {
     pageTree.restoreTree()
