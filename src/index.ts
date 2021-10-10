0a827d996e981d8bcedbabcb0cd2f24b5c680215
nogahighland <noga.highland@gmail.com>
2021-10-10 21:47:25 +0900

    add font-awesome

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue, { VNode } from 'vue'
 
 import App from '@/components/app.vue'
 import { addTargetBlank, hideOriginalPageTree } from '@/lib/'
+
+library.add(faSyncAlt)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 const appElement = document.createElement('div')
 appElement.id = 'page-tree-incremental-search'
