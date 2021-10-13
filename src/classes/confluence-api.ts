8b68357ee0e5265ec67bc2128bfc6b4ab0c7e279
nogahighland <noga.highland@gmail.com>
2021-10-13 18:20:34 +0900

    fix compile error

--- src/classes/confluence-api.ts
+++ src/classes/confluence-api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios'
 import { DOMUtils } from '@/classes/utils/'
 
 export class ConfluenceApi {
-  static async request(): Promise<AxiosResponse> {
+  static async request(): Promise<AxiosResponse<string>> {
     return axios.get(DOMUtils.treeUrl)
   }
 }
