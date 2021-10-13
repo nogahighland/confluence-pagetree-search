d5f7c2c04a63db34e0557314d1a9a897042a49a1
nogahighland <noga.highland@gmail.com>
2021-10-13 18:42:30 +0900

    omit timestamp for nodes

--- tests/classes/utils/compressing-utils.test.ts
+++ tests/classes/utils/compressing-utils.test.ts
@@ -5,9 +5,9 @@ import { CompressingUtils } from '@/classes/utils/compressing-utils'
 describe(CompressingUtils, () => {
   const buffer = Buffer.from(JSON.stringify(root))
   it('', () => {
-    expect(buffer.byteLength).toEqual(464)
+    expect(buffer.byteLength).toEqual(400)
     const compressed = CompressingUtils.deflate(root)
-    expect(Buffer.from(compressed).byteLength).toEqual(194)
+    expect(Buffer.from(compressed).byteLength).toEqual(193)
     const restored = CompressingUtils.inflate(compressed)
     expect(restored).toEqual(root)
   })
