096dd4f38f35fe75e5257688168b04ec3dfb0abf
nogahighland <noga.highland@gmail.com>
2022-03-14 19:47:47 +0900

    add breadcrumb to node property

--- src/classes/utils/tree-utils.ts
+++ src/classes/utils/tree-utils.ts
@@ -1,3 +1,4 @@
+import { pageTree } from '@/store/page-tree'
 import { Root, Tree } from '@/types'
 
 export class TreeUtils {
@@ -11,5 +12,33 @@ export class TreeUtils {
       ...tree.children.flatMap(child => this.extractNodeList(child))
     ]
     return nodeList
+  }
+
+  static addBreadcrumb(root: Root | Tree): void {
+    if (!root.children) {
+      return
+    }
+    root.children.forEach(tree => {
+      tree.breadcrumb = [
+        ...this.getParents(tree).map(p => p.linkText),
+        tree.linkText
+      ].join(' ')
+      this.addBreadcrumb(tree)
+    })
+  }
+
+  static getParents(tree: Tree): Tree[] {
+    const parents: Tree[] = []
+    if (!pageTree.root) {
+      return parents
+    }
+    let base: Root | Tree = pageTree.root
+    tree.address.forEach(i => {
+      /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+      base = base.children![i]
+      parents.push(base as Tree)
+    })
+    parents.pop()
+    return parents
   }
 }
